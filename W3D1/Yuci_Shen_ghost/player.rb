2ae748717c9ad12b0741660149850bdf6cf1f5aa
yshen901 <36720898+yshen901@users.noreply.github.com>
2019-09-07 17:42:20 -0700

    Fixed bugs and optimized UI

--- W3D1/Yuci_Shen_ghost/player.rb
+++ W3D1/Yuci_Shen_ghost/player.rb
@@ -8,12 +8,16 @@ class Player
 
     #gets a move from the player
     def get_move
-        p "Please enter guess (alpha only): "
+        print "Please enter guess (alpha only): "
         gets.chomp.downcase
     end
 
     #tells the player that the guess was wrong
     def alert_invalid_guess
-        puts "Guess is invalid"
+        puts "Guess is invalid, please try again"
+    end
+
+    def to_s
+        @name.to_s
     end
 end
