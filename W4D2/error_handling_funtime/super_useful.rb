23718720b06ff3bad7e96a0d1554af45e772a8b7
yshen901 <36720898+yshen901@users.noreply.github.com>
2019-09-10 15:02:44 -0700

    Finished error handling tasks

--- W4D2/error_handling_funtime/super_useful.rb
+++ W4D2/error_handling_funtime/super_useful.rb
@@ -1,30 +1,51 @@
 # PHASE 2
 def convert_to_int(str)
   Integer(str)
+rescue TypeError #this works somehow
+  nil
+rescue ArgumentError
+  nil
 end
 
 # PHASE 3
 FRUITS = ["apple", "banana", "orange"]
 
+class CoffeeError < StandardError ; end
+
+
 def reaction(maybe_fruit)
   if FRUITS.include? maybe_fruit
     puts "OMG, thanks so much for the #{maybe_fruit}!"
+  elsif maybe_fruit == "coffee"
+    raise CoffeeError, "Bleh, I don't like coffee, try again"
   else 
     raise StandardError 
   end 
 end
 
 def feed_me_a_fruit
-  puts "Hello, I am a friendly monster. :)"
+  begin
+    puts "Hello, I am a friendly monster. :)"
 
-  puts "Feed me a fruit! (Enter the name of a fruit:)"
-  maybe_fruit = gets.chomp
-  reaction(maybe_fruit) 
-end  
+    puts "Feed me a fruit! (Enter the name of a fruit:)"
+    maybe_fruit = gets.chomp
+    reaction(maybe_fruit) 
+  rescue CoffeeError
+    puts "This isn't a fruit, but I'll give you another chance"
+    retry
+  end
+end 
 
 # PHASE 4
+class YearsKnownError < StandardError; end
+class FriendNameError < StandardError; end
+class PastimeError < StandardError; end
 class BestFriend
   def initialize(name, yrs_known, fav_pastime)
+    raise YearsKnownError, "You haven't known me long enough to be a best friend. Come back in a few years" if yrs_known < 5
+    raise PastimeError, "You need a pastime" if fav_pastime.length <= 0
+    raise FriendNameError, "You don't have a name?" if name.length <= 0
+
     @name = name
     @yrs_known = yrs_known
     @fav_pastime = fav_pastime
