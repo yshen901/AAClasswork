31521c399f430790d220181eed1edb3cf2181b2e
yshen901 <36720898+yshen901@users.noreply.github.com>
2019-09-12 17:01:30 -0700

    Started Card spec

--- W4D4/poker/spec/card_spec.rb
+++ W4D4/poker/spec/card_spec.rb
@@ -1,6 +1,9 @@
 require 'card'
 
 RSpec.describe Card do
-
-
+  describe "#initialize" do
+    it "should should take in two symbols, a value and a suit, as args" do
+      expect { Card.new(:K, :S) }.to_not raise_error
+    end
+  end
 end
