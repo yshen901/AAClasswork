a3b9be956d93d80a188d55298be9e03f517bbf45
YUCI SHEN <36720898+yshen901@users.noreply.github.com>
2019-09-20 15:15:36 -0700

    Add skeleton and complete associations and verifications

---
+++ W5D5/associations_exercise/app/models/enrollment.rb
@@ -0,0 +1,24 @@
+# == Schema Information
+#
+# Table name: enrollments
+#
+#  id         :bigint           not null, primary key
+#  course_id  :integer
+#  student_id :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Enrollment < ApplicationRecord
+  validates :course_id, :student_id, presence: true
+
+  belongs_to :user,
+    primary_key: :id,
+    foreign_key: :student_id,
+    class_name: :User
+  
+  belongs_to :course,
+    primary_key: :id,
+    foreign_key: :course_id,
+    class_name: :Course
+end
