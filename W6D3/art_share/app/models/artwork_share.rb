ed1762190b1997167f2e6fcab35dc48dc9178007
yshen901 <36720898+yshen901@users.noreply.github.com>
2019-09-26 09:10:06 -0700

    Beging and complete migrations/models for art_share project

---
+++ W6D3/art_share/app/models/artwork_share.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: artwork_shares
+#
+#  id         :bigint           not null, primary key
+#  artwork_id :bigint           not null
+#  viewer_id  :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class ArtworkShare < ApplicationRecord
+  validates :artwork_id, :viewer_id, presence: true
+end
---
+++ W6D3/art_share/test/models/artwork_test.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: artworks
+#
+#  id         :bigint           not null, primary key
+#  title      :string           not null
+#  image_url  :text             not null
+#  artist_id  :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+require 'test_helper'
+
+class ArtworkTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
