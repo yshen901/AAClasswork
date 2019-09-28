e186bed093d11f951cc2c7b0dec536ef601ca59b
App Academy Student <student@appacademy.io>
2019-09-27 18:18:27 -0700

    Start 99_cats and complete models, migrations, and index/edit/show/new routes

---
+++ W6D5/cats_99/app/models/cat.rb
@@ -0,0 +1,19 @@
+class Cat < ApplicationRecord
+  # include ActionView::DateHelpers
+  VALID_COLORS = %w(brown black orange white grey).freeze
+  validates :birth_date, :color, :name, :sex, :description, presence: true 
+  validates :sex, inclusion: { in: ['M', 'F'], message: "%{value} is not a valid sex" }
+  validates :color, inclusion: { in: VALID_COLORS, message: "%{value} is not a valid color" }
+
+   
+  def age 
+    # select_date(Time.now, order: [:year, :month, :date], date_separator: '/')
+    # Time.now + 4.days
+    # check if current month is >= the month
+    # Extract the year
+    # Extract day
+    # extract month
+    404
+  end
+
+end
