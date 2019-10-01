0f8bb90cf77e111928d111c791cd5a73d2727301
App Academy Student <student@appacademy.io>
2019-09-30 17:01:25 -0700

    Complete Session controller and views

--- W7D1/cats_99/app/controllers/sessions_controller.rb
+++ W7D1/cats_99/app/controllers/sessions_controller.rb
@@ -1,2 +1,19 @@
 class SessionsController < ApplicationController
+  def new
+    render :new
+  end 
+
+  def create
+    # verify username and password.
+    @user = User.find_by_credentials(
+      params[:user][:user_name], 
+      params[:user][:password] 
+    ) 
+    if @user 
+      session[:session_token] = @user.reset_session_token!
+      redirect_to cats_url
+    else
+      render json: "Invalid Login", status: 402
+    end 
+  end
 end
