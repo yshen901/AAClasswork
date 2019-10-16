2e0dcca8147473dbb419938af860a4c084e2c5ab
yshen901 <36720898+yshen901@users.noreply.github.com>
2019-10-16 16:31:38 -0700

    Move winner/loser logic into css classes

--- W9D3/ticTacToe/src/ttt-view.js
+++ W9D3/ticTacToe/src/ttt-view.js
@@ -9,13 +9,15 @@ class View {
   bindEvents() {
     let that = this;
     
+    //Use $ to find a jQuery object
+    //.on creates a listener essentially for everything within the scope of the jQuery object
     $(".game-board").on('click', (e) => {
-      const square = e.target;
+      const square = e.target; //e.target is an HTML element, not a jQuery
       const pos = [square.id % 3, Math.floor(square.id / 3)];
       
       try {
         that.game.playMove(pos);
-        that.makeMove($(square));
+        that.makeMove($(square)); //use $ to convert an HTML element to a jQuery element
       }
       catch(MoveError){
         alert("Invalid move! Try again.");
@@ -26,41 +28,27 @@ class View {
   }
 
   unbindEvents(){
-    $(".game-board").unbind();
+    $(".game-board").unbind(); //removes the listener from the jQuery object
   }
 
   makeMove($square) {
+    //Use .attr() to get the value of an attribute
     const pos = [$square.attr('id') % 3, Math.floor($square.attr('id') / 3)];
-    $square.addClass(this.game.getMark(pos));
+    $square.addClass(this.game.getMark(pos)); //removeClass() to do the opposite
   }
 
   endGame() {
     this.unbindEvents();
     this.$el.append("<h3>You win, " + this.game.winner() + "!</h3>");
-    
-    let pos, mark, $square;
-    let winner = this.game.winner();
-    let squares = Array.from($('.ttt-square'));
-
-    for(let i = 0; i < squares.length; i++){
-      pos = [squares[i].id % 3, Math.floor(squares[i].id / 3)];
-      mark = this.game.getMark(pos); //returns the mark from the array representation of the board
-      $square = $(squares[i]);
-
-      if (mark === winner)
-        $square.addClass('winner');
-      else
-        $square.addClass('loser');
-
-      $square.removeClass('hoverable');
-    }
+    $(".ttt").addClass("gameover winner-" + this.game.winner());
   }
 
   setupBoard() {
     this.$el.append("<ul class='game-board'></ul>");
     let $gameBoard = $(".game-board");
     for (let i = 0; i < 9; i++){
-      $("<li class='ttt-square hoverable' id=" + i + "></li>").appendTo($gameBoard);
+      //adds element A to element B...potentially the other way doesn't work here w/ ($gameBoard).append
+      $("<li class='ttt-square' id=" + i + "></li>").appendTo($gameBoard); 
     }
   }
 }
