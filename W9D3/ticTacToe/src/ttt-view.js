3628b01865a0529ebc0f553f8283db71448a5ea6
yshen901 <36720898+yshen901@users.noreply.github.com>
2019-10-16 17:01:24 -0700

    Remove id and add position information to data

--- W9D3/ticTacToe/src/ttt-view.js
+++ W9D3/ticTacToe/src/ttt-view.js
@@ -12,12 +12,12 @@ class View {
     //Use $ to find a jQuery object
     //.on creates a listener essentially for everything within the scope of the jQuery object
     $(".game-board").on('click', (e) => {
-      const square = e.target; //e.target is an HTML element, not a jQuery
-      const pos = [square.id % 3, Math.floor(square.id / 3)];
+      const $square = $(e.target); //e.target is an HTML element, not a jQuery...use $ to convert
+      const pos = $square.data("pos");
       
       try {
         that.game.playMove(pos);
-        that.makeMove($(square)); //use $ to convert an HTML element to a jQuery element
+        that.makeMove($square);
       }
       catch(MoveError){
         alert("Invalid move! Try again.");
@@ -32,23 +32,31 @@ class View {
   }
 
   makeMove($square) {
+    console.log($square);
     //Use .attr() to get the value of an attribute
-    const pos = [$square.attr('id') % 3, Math.floor($square.attr('id') / 3)];
+    const pos = $square.data("pos");
     $square.addClass(this.game.getMark(pos)); //removeClass() to do the opposite
   }
 
   endGame() {
     this.unbindEvents();
     this.$el.append("<h3>You win, " + this.game.winner() + "!</h3>");
+
+    /* Use classes as toggles, add a gameover class to toggle this behavior 
+       Also use classes to design simple logic */
     $(".ttt").addClass("gameover winner-" + this.game.winner());
   }
 
   setupBoard() {
     this.$el.append("<ul class='game-board'></ul>");
     let $gameBoard = $(".game-board");
+    let $li;
     for (let i = 0; i < 9; i++){
+
+      $li = $("<li class='ttt-square'></li>")
+      $li.data("pos", [i % 3, Math.floor(i / 3)]);
       //adds element A to element B...potentially the other way doesn't work here w/ ($gameBoard).append
-      $("<li class='ttt-square' id=" + i + "></li>").appendTo($gameBoard); 
+      $li.appendTo($gameBoard); 
     }
   }
 }
